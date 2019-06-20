4e4a8370d4a360c70dc2bce3c6cb1946ba987deb
Oluwatomisin Lalude <oluwatomisin1605@gmail.com>
2019-06-20 14:34:21 +0100

    [Feature 166816464] Modify route

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,14 +5,14 @@ import express from 'express';
 import dotenv from 'dotenv';
 
 import authRouter from './AuthRoutes';
-import carRouter from './CarRoutes';
-import flagRouter from './FlagRoutes';
+// import carRouter from './CarRoutes';
+// import flagRouter from './FlagRoutes';
 
 dotenv.config();
 const router = express.Router();
 
 router.use('/auth', authRouter);
-router.use('/car', carRouter);
-router.use('/flag', flagRouter);
+// router.use('/car', carRouter);
+// router.use('/flag', flagRouter);
 
 export default router;
