ee3a5f718f9cbcebf5a8c0ed142b45df89642b45
Oluwatomisin Lalude <oluwatomisin1605@gmail.com>
2019-07-16 06:57:04 +0100

    Modify schema

--- src/database/tables/schema.js
+++ src/database/tables/schema.js
@@ -5,13 +5,13 @@ import debug from 'debug';
 import authTable from './authTable';
 import carTable from './carTable';
 import orderTable from './orderTable';
-import pool from '../index';
+import Client from '../../config/databaseConfig';
 
 const schema = async () => {
   try {
-    await pool.query(authTable);
-    await pool.query(carTable);
-    await pool.query(orderTable);
+    await Client.query(authTable);
+    await Client.query(carTable);
+    await Client.query(orderTable);
   } catch (err) {
     if (err) debug(err);
   }
