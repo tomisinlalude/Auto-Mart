3c79f44da665e5a706f8068ca159ce2aaa5f4970
Oluwatomisin Lalude <oluwatomisin1605@gmail.com>
2019-07-16 15:13:39 +0100

    Modify authModel

--- src/database/models/authModel.js
+++ src/database/models/authModel.js
@@ -1,6 +1,6 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable class-methods-use-this */
-// eslint-disable-next-line no-unused-vars
+/* eslint-disable camelcase */
 
 import client from '../../config/databaseConfig';
 
@@ -9,7 +9,7 @@ import client from '../../config/databaseConfig';
  */
 
 class authModel {
-  async checkAll(email) {
+  async checkEmail(email) {
     const query = {
       text: 'SELECT * FROM Users ORDER BY id ASC;',
       values: [email],
@@ -18,17 +18,27 @@ class authModel {
     return rows[0];
   }
 
-  async createUser(first_name, last_name, email, password, confirm_password, address) {
+  async createUser(first_name, last_name, email, passwordHash, address) {
     const query = {
       text: `INSERT INTO Users
                 (first_name, last_name, email, password, confirm_password, address)
                 VALUES($1, $2, $3, $4, $5, $6, $7)
                 RETURNING id, firstName, lastName, email;`,
-      values: [first_name, last_name, email, password, confirm_password, address],
+      values: [first_name, last_name, email, passwordHash, address],
     };
 
     const { rows } = await client.query(query);
     return rows[0];
+  }
+
+  async resetPassword(user_id, passwordHash) {
+    const query = {
+      text: `UPDATE users SET password_hash = $2
+            WHERE user_id = $1; `,
+      values: [user_id, passwordHash],
+    };
+    const { rows } = await client.query(query);
+    return rows;
   }
 }
 
