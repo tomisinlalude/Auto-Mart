12f8ad1b68d6979ade9bf58454a2f87a225eea91
Oluwatomisin Lalude <oluwatomisin1605@gmail.com>
2019-07-16 06:48:41 +0100

    Modify Orders Schema

--- src/database/tables/orderTable.js
+++ src/database/tables/orderTable.js
@@ -1,11 +1,12 @@
 /* eslint-disable linebreak-style */
 const orderTable = `CREATE TABLE IF NOT EXISTS Orders(
-  id SERIAL PRIMARY KEY,
-  buyer VARCHAR NOT NULL,
-  carId INTEGER REFERENCES car (id),
-  createdOn TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  status VARCHAR NOT NULL,
-  priceOffered NUMERIC NOT NULL
+  order_id SERIAL,
+    buyer_id INTEGER REFERENCES users(user_id) ON DELETE CASCADE NOT NULL,
+    car_id INTEGER REFERENCES cars(car_id) ON DELETE CASCADE NOT NULL,
+    price_offered INTEGER NOT NULL,
+    price INTEGER NOT NULL,
+    status VARCHAR(10) DEFAULT 'pending',
+    created_on DATE DEFAULT NOW()
 );
 `;
 
