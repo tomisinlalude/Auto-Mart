5f51950995950761a77d6e8419321bed058c9b52
Oluwatomisin Lalude <oluwatomisin1605@gmail.com>
2019-07-16 16:00:33 +0100

    Add authRoutes and carRoutes

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,12 +4,16 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
+import authRouter from './AuthRoutes';
+import carRouter from './carRoutes';
 import orderRouter from './OrderRoutes';
 import uploadRouter from './UploadRoutes';
 
 dotenv.config();
 const router = express.Router();
 
+router.use('/auth', authRouter);
+router.use('/car', carRouter);
 router.use('/order', orderRouter);
 router.use('/uploads', uploadRouter);
 
