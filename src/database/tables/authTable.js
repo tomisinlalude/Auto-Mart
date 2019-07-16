478c4d629c20b04e85d92b7ce72d04bebcf36f12
Oluwatomisin Lalude <oluwatomisin1605@gmail.com>
2019-07-16 06:29:42 +0100

    Modify tables

--- src/database/tables/authTable.js
+++ src/database/tables/authTable.js
@@ -1,12 +1,11 @@
 /* eslint-disable linebreak-style */
 const authTable = `
   CREATE TABLE IF NOT EXISTS Users(
-    id SERIAL PRIMARY KEY,
+    user_id SERIAL PRIMARY KEY,
     token VARCHAR NOT NULL,
-    firstName VARCHAR (50) NOT NULL,
-    lastName VARCHAR (50) NOT NULL,
+    first_name VARCHAR (50) NOT NULL,
+    last_name VARCHAR (50) NOT NULL,
     email VARCHAR (254) UNIQUE NOT NULL,
-    phoneNumber VARCHAR (15) UNIQUE NOT NULL,
     password VARCHAR NOT NULL,
     address VARCHAR NOT NULL,
     is_admin BOOLEAN
--- src/database/tables/carTable.js
+++ src/database/tables/carTable.js
@@ -2,16 +2,14 @@
 
 const carTable = `
   CREATE TABLE IF NOT EXISTS Cars(
-    carId SERIAL PRIMARY KEY,
+    car_id SERIAL PRIMARY KEY,
     owner VARCHAR NOT NULL,
-    createdOn TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     state VARCHAR NOT NULL,
     status VARCHAR NOT NULL,
     price NUMERIC NOT NULL,
-    make VARCHAR NOT NULL,
     manufacturer VARCHAR NOT NULL,
     model VARCHAR NOT NULL,
-    bodyType VARCHAR NOT NULL
+    body_type VARCHAR NOT NULL
   );
 `;
 
