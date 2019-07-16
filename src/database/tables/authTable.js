af81aa388862f270b207ac352b463fa30cf708f1
Oluwatomisin Lalude <oluwatomisin1605@gmail.com>
2019-07-16 06:45:30 +0100

    Modify Auth Schema

--- src/database/tables/authTable.js
+++ src/database/tables/authTable.js
@@ -2,13 +2,12 @@
 const authTable = `
   CREATE TABLE IF NOT EXISTS Users(
     user_id SERIAL PRIMARY KEY,
-    token VARCHAR NOT NULL,
-    first_name VARCHAR (50) NOT NULL,
-    last_name VARCHAR (50) NOT NULL,
-    email VARCHAR (254) UNIQUE NOT NULL,
-    password VARCHAR NOT NULL,
-    address VARCHAR NOT NULL,
-    is_admin BOOLEAN
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(254) UNIQUE NOT NULL,
+    password_hash VARCHAR(150) NOT NULL,
+    address VARCHAR(120) NOT NULL,
+    is_admin BOOLEAN DEFAULT false
   );
 `;
 
