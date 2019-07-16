3d1023395e2a9e1ec8636dcb80010a8ce0f41354
Oluwatomisin Lalude <oluwatomisin1605@gmail.com>
2019-07-16 06:30:18 +0100

    Modify jwt

--- src/middlewares/jwt.js
+++ src/middlewares/jwt.js
@@ -2,7 +2,11 @@
 import jwt from 'jsonwebtoken';
 
 const secretKey = process.env.SECRET_KEY;
-export const generateToken = payload => jwt.sign(payload, secretKey, { expiresIn: '240h' });
+export const generateToken = (user_id, email) => {
+  const token = jwt.sign({user_id, email}, secretKey, { expiresIn: 84600 });
+  return token;
+};
+
 export const verifyToken = (req, res, next) => {
   const token = req.headers.authorization;
   if (!token) {
@@ -13,12 +17,24 @@ export const verifyToken = (req, res, next) => {
   }
   try {
     const release = jwt.verify(token, secretKey);
-    req.body.id = release.id;
+    req.body.id = release.user_id;
+    req.user = release;
     return next();
   } catch (err) {
     return res.status(400).json({
       success: false,
       message: 'This token is not valid',
     });
+  }
+};
+
+export const getUserFromToken = async (token) => {
+  try {
+      const release = jwt.verify(token, secretKey);
+      const { email } = release;
+      const user = await authModel.checkAll(email);
+      return user;
+  } catch (error) {
+      return false;
   }
 };
